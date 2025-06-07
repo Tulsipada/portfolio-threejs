6b3eba8eb98650c4a302e8396b9a7cc49f711996
Tulsipada Das <tulsipada55@gmail.com>
2025-06-07 15:20:16 +0530

    update project

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // 👈 IMPORTANT for custom domain (GitHub Pages)
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -20,3 +19,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
